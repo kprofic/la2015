9860ff4f88d8cb984e983ad56061785d5f21e444
Krzysztof Profic <kpr@trifork.com>
2014-12-14 20:08:29 +0100

    Initial Commit

---
+++ Voter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voter
+//
+//  Created by Krzysztof Profic on 14/12/14.
+//  Copyright (c) 2014 Lambda Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
